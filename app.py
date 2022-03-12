be42e8538678e25e10da1087856fba858cfde817
Vitor Hugo <v02hx10@gmail.com>
2022-03-12 17:40:34 -0300

    adding 'app.py'

---
+++ app.py
@@ -0,0 +1,97 @@
+from fastapi import FastAPI, Path, HTTPException, status
+from typing import Optional
+from pydantic import BaseModel
+
+
+"""
+FastAPI is a Python framework for building HTTP APIs.
+
+This is a basic project for a future FaRM Stack project.
+THIS IS NOT A REAL API. just a demo. Testing some stuff.
+the basic concept is a very simple CRUD api.
+"""
+# ============== CONSTANTS ==============
+
+
+class Item(BaseModel):
+    Name: str
+    Price: float
+    is_offer: bool = True
+    quantity: int = 1
+    color: Optional[str] = None
+
+
+class ItemUpdate(BaseModel):
+    name: Optional[str] = None
+    Price: Optional[float] = None
+    is_offer: Optional[bool] = None
+    quantity: Optional[int] = None
+    color: Optional[str] = None
+
+
+app = FastAPI()
+
+inventory = {}
+
+
+@app.get('/')
+def index():
+    return {"message": "Hello World Test"}
+
+
+@app.get("/about")
+def about():
+    return {"message": "This is a Test for a future project"}
+
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(None,
+                                 description="ID of the item to return",
+                                 title="Item ID", gt=0)):
+    return inventory.get(item_id)
+
+
+@app.get("/get-item-by-name")
+def get_item(name: str):
+    for item_id in inventory:
+        if inventory[item_id].name == name:
+            return inventory[item_id]
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.get("/get-all-items")
+def get_all_items():
+    return inventory
+
+
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item: Item):
+    if item_id in inventory:
+        if item.name != None:
+            inventory[item_id].name = item.name
+        if item.Price != None:
+            inventory[item_id].Price = item.Price
+        if item.is_offer != None:
+            inventory[item_id].is_offer = item.is_offer
+        if item.quantity != None:
+            inventory[item_id].quantity = item.quantity
+        if item.color != None:
+            inventory[item_id].color = item.color
+
+        return inventory[item_id]
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.post("/add-item")
+def add_item(item: Item):
+    item_id = max(inventory.keys()) + 1
+    inventory[item_id] = item
+    return inventory[item_id]
+
+
+@app.delete("/delete-item/{item_id}")
+def delete_item(item_id: int):
+    if item_id in inventory:
+        del inventory[item_id]
+        return {"message": "Item deleted"}
+    raise HTTPException(status_code=404, detail="Item not found")
