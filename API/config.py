67b5560d7e1cb15e6499f272056aa2122200c3c2
Vitor Hugo <v02hx10@gmail.com>
2022-03-15 01:06:52 -0300

    Fix: Mongo Connection

--- API/config.py
+++ API/config.py
@@ -1,27 +1,31 @@
+
 from pymongo import MongoClient
 
 # ============== [CONSTANTS] ==============
-MONGO_DB_NAME = "Tests"
-MONGO_COLLECTION_NAME = "Itens_Store"
+MONGO_DB_NAME = "myFirstDatabase"
+MONGO_COLLECTION_NAME = "Tests"
+MONGO_COLLECTION_DOCS = "Itens_Store"
 
 MONGO_USER = "m001-student"
 MONGO_PASSWORD = "m001-mongodb-basics"
-MONGO_URL = "mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@sandbox.iwqks.mongodb.net/{MONGO_DB_NAME}?retryWrites=true&w=majority"
+MONGO_URI = f'mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@sandbox.iwqks.mongodb.net/{MONGO_DB_NAME}?retryWrites=true&w=majority'
 # ========================================
 
 
-def connect():
+def connect() -> MongoClient:
     """
     Try to Connect to MongoDB
     """
     try:
-        client = MongoClient(MONGO_URL, 27017, serverSelectionTimeoutMS=1000)
-        client.server_info()
+        client = MongoClient(MONGO_URI)
+        assert client is not None
+        print(f"Connected to MongoDB at {MONGO_URI}")
 
-        db = client[MONGO_DB_NAME]
-        collection = db[MONGO_COLLECTION_NAME]
+        return client
+
     except Exception as e:
         print("ERROR: " + str(e))
         return None
 
-    return collection
+
+connect()
