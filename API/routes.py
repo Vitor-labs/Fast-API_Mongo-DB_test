098b01d62a2ed843147deb4e2fc20831d725bb4d
Vitor Hugo <v02hx10@gmail.com>
2022-03-20 06:41:43 -0300

    REFAC: Routes testeds

--- API/routes.py
+++ API/routes.py
@@ -1,112 +1,146 @@
 from fastapi import APIRouter
+from bson.objectid import ObjectId
 from models import Item, ItemUpdate
 from config import connect
+from util import serial_item, serial_items, serial_item_update, serial_items_update
 
 itens_routes = APIRouter()
 
 CONN_ERROR = {"error": "Connection error"}
+CREATE_ERROR = {"error": "Create error"}
+READ_ERROR = {"error": "Read error"}
+UPDATE_ERROR = {"error": "Update error"}
+DELETE_ERROR = {"error": "Delete error"}
 
 
 # =================== [CREATES] ===================
 @itens_routes.post("/")
-def create_item(item: Item):
+async def create_item(item: Item):  # Tested OK
     conn = connect()
     if conn is None:
         return CONN_ERROR
 
-    item_id = conn.Item_Store.insert_one(item.dict()).inserted_id
-    return {"item_id": str(item_id)}
+    try:
+        _id = conn.insert_one(dict(item))
+        data = serial_items(conn.find({"_id": _id.inserted_id}))
+        return {"Status": "OK", "item": data}
+
+    except Exception as e:
+        print(e)
+        return CREATE_ERROR
 # =================== [CREATES] ===================
 
 
 # ==================== [READS] ====================
 @itens_routes.get("/")
-def get_all():
+async def get_all():  # Tested OK
     conn = connect()
     if conn is None:
         return CONN_ERROR
 
-    return conn.find({})
+    try:
+        items = conn.find()
+        return {"Status": "OK", "Items": serial_items(items)}
+
+    except Exception as e:
+        print(e)
+        return READ_ERROR
 
 
 @itens_routes.get("/{item_name}")
-def get_by_name(item_name: str):
+async def get_by_name(item_name: str):  # Tested OK
     conn = connect()
     if conn is None:
         return CONN_ERROR
 
-    return conn.find_one({"name": item_name})
+    try:
+        items = conn.find({"name": item_name})
+        if items is None:
+            return {"Status": "OK", "Items": "No item found"}
+        return {"Status": "OK", "Items": serial_items(items)}
+    except Exception as e:
+        print(e)
+        return READ_ERROR
 
 
 @itens_routes.get("/{item_id}")
-def get_item(item_id: int):
+async def get_item(item_id: str):  # Tested No Return
     conn = connect()
     if conn is None:
         return CONN_ERROR
 
-    return conn.find_one({"id": item_id})
+    try:
+        item = serial_item(conn.find_one({"_id": ObjectId(item_id)}))
+        if item is None:
+            return {"Status": "Error", "Error": "Item not found"}
+        return {"Status": "OK", "Item": item}
+    except Exception as e:
+        print(e)
+        return READ_ERROR
 # ==================== [READS] ====================
 
 
 # =================== [UPDATES] ===================
 @itens_routes.put("/{item_id}")
-def update_item(item_id: int, item: ItemUpdate):
+async def update_item(item_id: str, item: ItemUpdate):  # Tested OK
     conn = connect()
     if conn is None:
         return CONN_ERROR
 
-    conn.update_one({"id": item_id}, {"$set": item.dict()})
-    return {"message": "Item {item_id} updated"}
+    try:
+        conn.update_one({"_id": ObjectId(item_id)}, {"$set": dict(item)})
+        item = serial_item_update(conn.find_one({"_id": ObjectId(item_id)}))
+        return {"Status": "OK", "Item": item}
+
+    except Exception as e:
+        print(e)
+        return UPDATE_ERROR
 
 
 @itens_routes.put("/{item_name}")
-def update_by_name(name: str, item: Item):
+async def update_by_name(name: str, item: Item):  # Tested Fail in $set
     conn = connect()
     if conn is None:
         return CONN_ERROR
 
-    conn.update_one({"name": name}, {"$set": item.dict()})
-    return {"message": "Item {name} updated"}
+    try:
+        conn.find_one_and_update({"name": name}, {"$set": dict(item)})
+        item = serial_item(conn.find_one({"name": name}))
+        return {"Status": "OK", "Item": item}
 
-
-@itens_routes.patch("/{item_id}")
-def update_item_partial(item_id: int, item: ItemUpdate):
-    conn = connect()
-    if conn is None:
-        return CONN_ERROR
-
-    if item.name:
-        conn.update_one({"id": item_id}, {"$set": {"name": item.name}})
-    if item.color:
-        conn.update_one({"id": item_id}, {"$set": {"color": item.color}})
-    if item.price:
-        conn.update_one({"id": item_id}, {"$set": {"price": item.price}})
-    if item.quantity:
-        conn.update_one({"id": item_id}, {"$set": {"quantity": item.quantity}})
-    if item.is_offer:
-        conn.update_one({"id": item_id}, {"$set": {"is_offer": item.is_offer}})
-
-    return {"message": "Item {item_id} updated"}
+    except Exception as e:
+        print(e)
+        return UPDATE_ERROR
 # =================== [UPDATES] ===================
 
 
 # =================== [DELETES] ===================
 @itens_routes.delete("/{item_id}")
-def delete_item(item_id: int):
+def delete_item(item_id: str):  # Tested OK
     conn = connect()
     if conn is None:
         return CONN_ERROR
 
-    conn.delete_one({"id": item_id})
-    return {"message": f"Item {item_id} deleted"}
+    try:
+        conn.delete_one({"_id": ObjectId(item_id)})
+        return {"Status": "OK"}
+
+    except Exception as e:
+        print(e)
+        return DELETE_ERROR
 
 
 @itens_routes.delete("/{item_name}")
-def delete_by_name(item_name: str):
+def delete_by_name(item_name: str):  # Tested Erro on valid objectid, same as update
     conn = connect()
     if conn is None:
         return CONN_ERROR
 
-    conn.delete_one({"name": item_name})
-    return {"message": f"Item {item_name} deleted"}
+    try:
+        conn.delete_one({"name": item_name})
+        return {"Status": "OK"}
+
+    except Exception as e:
+        print(e)
+        return DELETE_ERROR
 # =================== [DELETES] ===================
