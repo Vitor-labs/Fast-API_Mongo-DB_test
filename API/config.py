ad3a35e3f763585251e73ad50535529fecab106f
Vitor Hugo <v02hx10@gmail.com>
2022-03-20 06:40:33 -0300

    REFAC: *U SHALL NOT SEE*

--- API/config.py
+++ API/config.py
@@ -3,10 +3,9 @@ from pymongo import MongoClient
 # ============== [CONSTANTS] ==============
 MONGO_DB_NAME = "myFirstDatabase"
 MONGO_COLLECTION_NAME = "Tests"
-MONGO_COLLECTION_DOCS = "Itens_Store"
 
-MONGO_USER = "m001-student"
-MONGO_PASSWORD = "m001-mongodb-basics"
+MONGO_USER = "lord of the colars"
+MONGO_PASSWORD = "its a mistake"
 MONGO_URI = f'mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@sandbox.iwqks.mongodb.net/{MONGO_DB_NAME}?retryWrites=true&w=majority'
 # ========================================
 
